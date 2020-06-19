3ba7453cd47a072811d8f5b3a435c6ecd9b305ee
ShabbirBiradar <biradarshabbir@gmail.com>
2020-06-20 00:56:35 +0530

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/shopping_cart/precache-manifest.0a60b7d98c5d82b8d0aa23afd16b473d.js"
+  "/shopping_cart/precache-manifest.858e9e548d8a09bbcb18d0e0d7a32c2f.js"
 );
 
 self.addEventListener('message', (event) => {
