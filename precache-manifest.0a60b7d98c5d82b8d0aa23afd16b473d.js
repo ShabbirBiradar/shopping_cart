40be0c2ada46d32c70303352ffc76269d0bcf3af
ShabbirBiradar <biradarshabbir@gmail.com>
2020-06-20 00:35:37 +0530

    Updates

---
+++ precache-manifest.0a60b7d98c5d82b8d0aa23afd16b473d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0a4365f650c0af802a8f67b924528dee",
+    "url": "/shopping_cart/index.html"
+  },
+  {
+    "revision": "4919d6e0bea4245b03a4",
+    "url": "/shopping_cart/static/css/main.53b53255.chunk.css"
+  },
+  {
+    "revision": "e214ec15a04bbfb70f7d",
+    "url": "/shopping_cart/static/js/2.03308dc0.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/shopping_cart/static/js/2.03308dc0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4919d6e0bea4245b03a4",
+    "url": "/shopping_cart/static/js/main.80ecad7d.chunk.js"
+  },
+  {
+    "revision": "b35a2a719d7c6ad482b5",
+    "url": "/shopping_cart/static/js/runtime-main.de0df27a.js"
+  }
+]);
