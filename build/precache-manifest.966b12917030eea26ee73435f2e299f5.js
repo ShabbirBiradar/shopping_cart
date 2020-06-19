4471dfea33589054f39b3ca0efb955edaeea435c
ShabbirBiradar <BiradarShabbir@gmail.com>
2020-06-19 23:51:22 +0530

    init

---
+++ build/precache-manifest.966b12917030eea26ee73435f2e299f5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "41dfe8d70d1eb52bce149d48bfe00bae",
+    "url": "/github.io/shopping-cart/index.html"
+  },
+  {
+    "revision": "e2742246f13b76ca14d9",
+    "url": "/github.io/shopping-cart/static/css/main.53b53255.chunk.css"
+  },
+  {
+    "revision": "e214ec15a04bbfb70f7d",
+    "url": "/github.io/shopping-cart/static/js/2.03308dc0.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/github.io/shopping-cart/static/js/2.03308dc0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e2742246f13b76ca14d9",
+    "url": "/github.io/shopping-cart/static/js/main.905b877e.chunk.js"
+  },
+  {
+    "revision": "fe5dfdf8e62aed55726f",
+    "url": "/github.io/shopping-cart/static/js/runtime-main.4a8b27d6.js"
+  }
+]);
